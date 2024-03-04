9f57f67862b1981b0a647b62328a878bbbc3c0e2
Ryan <r.fortune25@ncf.edu>
2024-03-04 13:36:50 -0500

    getting both paddle and food before boat
    -  still need to add transition to end screen
    - need to troubleshoot why booleans are turning true but not setting gameobject to false in conditional

---
+++ assignment 2 VR/Assets/Scripts/IslandObjectManager.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IslandObjectManager : MonoBehaviour
+{
+    // The whole point of this class is going to be to manage the current state of the various objects, and to then make the nessessary changes
+    // First we will get a text object to store the notification text. 
+    public Text text;
+    // Next we will have three booleans to manage the pickups
+    public Boolean paddlePickup = false;
+    public Boolean consumablesPickup = false;
+   
+
+
+    public void pickupPaddle()
+    {
+        paddlePickup = true;
+        Debug.Log("Paddle Picked up");
+    }
+    public void pickupConsumables()
+    {
+        consumablesPickup = true;
+        Debug.Log("Consumables = true now");
+    }
+
+
+
+    // Now we have to check the door
+    public void pickupBoat(GameObject Boat)
+    {
+        if (Boat == null)
+        {
+            Debug.LogError("Boat GameObject is null!");
+            return;
+        }
+
+        if (paddlePickup && consumablesPickup)
+        {
+            text.text = "You have everything you need!";
+            Boat.SetActive(false);
+        }
+        else
+        {
+            text.text = "I still need something for my voyage";
+        }
+    }
+
+}
